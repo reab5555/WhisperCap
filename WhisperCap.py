2e388b664215972db565b857b81fc41c3c6dca63
reab5555 <130560722+reab5555@users.noreply.github.com>
2024-03-23 14:22:13 +0200

    Update WhisperCap.py

--- WhisperCap.py
+++ WhisperCap.py
@@ -1,18 +1,13 @@
-import re
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
+import sys
 import os
 import math
-import srt
-import csv
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QFileDialog, QProgressBar, QCheckBox
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
+from moviepy.editor import VideoFileClip, AudioFileClip
+import tempfile
 from openai import OpenAI
-from moviepy.editor import *
-
+import srt
 import datetime
-from tqdm import tqdm
-
-
 
 
 # Initialize OpenAI API
@@ -20,122 +15,225 @@ api_key = "API_KEY_HERE"
 
 client = OpenAI(api_key=api_key)
 
-print('******** OpenAI Whisper Speech2Text - txt + srt - by REA.B ********')
+class TranscriptionThread(QThread):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal(str, str)
+    errorOccurred = pyqtSignal(str)
 
-# Set up the file dialog to select the input video or audio file
-print('Please select .mp4, .wav, or .mp3 file...')
-root = tk.Tk()
-root.withdraw()
-root.attributes("-top", True)
-# Allow selection of .mp4 (video files), and .wav, .mp3 (audio files)
-file_path = filedialog.askopenfilename(filetypes=[("Media files", "*.mp4;*.wav;*.mp3")])
-root.destroy()
+    def __init__(self, file_path, transcribe_to_text, transcribe_to_srt):
+        super().__init__()
+        self.file_path = file_path
+        self.transcribe_to_text = transcribe_to_text
+        self.transcribe_to_srt = transcribe_to_srt
+        self.stopped = False
 
-# Determine the file type
-file_extension = os.path.splitext(file_path)[1].lower()
+    def run(self):
+        file_extension = os.path.splitext(self.file_path)[1].lower()
+        output_folder = os.path.join(os.path.dirname(self.file_path), os.path.splitext(os.path.basename(self.file_path))[0] + " Transcription Results")
+        os.makedirs(output_folder, exist_ok=True)
+        video_duration = 0
 
-# Initialize variables for duration and chunk processing
-video_duration = 0
-n_chunks = 0
+        temp_files = []  # Keep track of temporary files
 
-# Handle different file types
-if file_extension == '.mp4':
-    video = VideoFileClip(file_path)
-    video_duration = video.duration
-elif file_extension in ['.wav', '.mp3']:
-    # For wav and mp3 files, directly use them without extracting audio
-    audio = AudioFileClip(file_path)  # Use AudioFileClip for audio files
-    video_duration = audio.duration
+        if file_extension == '.mp4':
+            video = VideoFileClip(self.file_path)
+            video_duration = video.duration
+        elif file_extension in ['.wav', '.mp3']:
+            audio = AudioFileClip(self.file_path)
+            video_duration = audio.duration
+        else:
+            self.errorOccurred.emit("Unsupported file type")
+            return
 
-# Create output folder
-output_name = os.path.splitext(os.path.basename(file_path))[0]
-output_folder = os.path.dirname(file_path)
-output_folder_name = f"{output_name} - transcription data"
-transcription_data_folder = os.path.join(output_folder, output_folder_name)
-os.makedirs(transcription_data_folder, exist_ok=True)
+        n_chunks = math.ceil(video_duration / 30)
+        transcription_txt = ""
+        transcription_srt = ""
+        last_index = 0
+        time_offset = 0
 
-# Split the media into 30-second chunks for processing
-n_chunks = math.ceil(video_duration / 30)
+        for i in range(n_chunks):
+            if self.stopped: break
 
-# Transcribe the video or audio chunks
-transcription_txt = ""
-transcription_srt = ""
-last_index = 0
-time_offset = 0
+            start = i * 30
+            end = min((i + 1) * 30, video_duration)
+            chunk_duration = end - start
 
-for i in tqdm(range(n_chunks), desc='Transcribing Media to Text'):
-    start = i * 30
-    end = (i + 1) * 30 if i < n_chunks - 1 else video_duration  # Adjust the duration of the last chunk
+            temp_file_path = os.path.join(output_folder, f"temp_audio_{i}.wav")
+            temp_files.append(temp_file_path)  # Add temp file path to the list
 
-    if file_extension == '.mp4':
-        audio_chunk = video.subclip(start, end).audio
-    elif file_extension in ['.wav', '.mp3']:
-        audio_chunk = audio.subclip(start, end)
+            audio_chunk = video.subclip(start, end).audio if file_extension == '.mp4' else audio.subclip(start, end)
+            audio_chunk.write_audiofile(temp_file_path, codec='pcm_s16le')
 
-    audio_chunk.write_audiofile("temp_audio.wav", codec='pcm_s16le')
+            with open(temp_file_path, "rb") as audio_file:
+                    if self.transcribe_to_text:
+                        response_txt = client.audio.transcriptions.create(
+                            file=audio_file,
+                            model="whisper-1",
+                            response_format="text",
+                        )
+                        # Assuming the response is not always a dictionary and might be a string directly
+                        if isinstance(response_txt, dict) and 'text' in response_txt:
+                            transcription_text_part = response_txt['text']
+                        elif isinstance(response_txt, str):
+                            transcription_text_part = response_txt
+                        else:
+                            # Handle unexpected response types, maybe log an error or throw an exception
+                            print("Unexpected response format from transcription service.")
+                            transcription_text_part = ""
 
-    with open("temp_audio.wav", "rb") as audio_file:
-        response = client.audio.transcriptions.create(
-            file=audio_file,
-            model="whisper-1",
-            response_format="text",
-        )
+                        transcription_txt += transcription_text_part
 
-    # Assuming the response is not always a dictionary and might be a string directly
-    if isinstance(response, dict) and 'text' in response:
-        transcription_text_part = response['text']
-    elif isinstance(response, str):
-        transcription_text_part = response
-    else:
-        # Handle unexpected response types, maybe log an error or throw an exception
-        print("Unexpected response format from transcription service.")
-        transcription_text_part = ""
+                    if self.transcribe_to_srt:
+                        with open(temp_file_path, "rb") as audio_file:
+                            response_srt = client.audio.transcriptions.create(
+                                file=audio_file,
+                                model="whisper-1",
+                                response_format="srt",
+                            )
 
-    transcription_txt += transcription_text_part
+                        # Directly use response_srt if it's already a string, which is expected for SRT content
+                        if isinstance(response_srt, str):
+                            srt_content = response_srt
+                        else:
+                            print("Unexpected SRT response format.")
+                            srt_content = ""
 
-    print(transcription_txt)
+                        # Parse and adjust the SRT content as needed
+                        segments_list = list(srt.parse(srt_content))
+                        for segment in segments_list:
+                            segment.index += last_index
+                            segment.start += datetime.timedelta(seconds=time_offset)
+                            segment.end += datetime.timedelta(seconds=time_offset)
+                            segment.start = segment.start - datetime.timedelta(microseconds=segment.start.microseconds)
+                            segment.end = segment.end - datetime.timedelta(microseconds=segment.end.microseconds)
 
-    # Save the transcription as a .txt file
-    txt_file_path = os.path.join(transcription_data_folder, f"{output_name} - text.txt")
-    with open(txt_file_path, "w", encoding='utf-8') as f:
-        f.write(transcription_txt)
+                        modified_srt = srt.compose(segments_list)
+                        transcription_srt += modified_srt
+                        last_index = segments_list[-1].index + 1
+                        time_offset += 30
 
-    # Transcribe to SRT
-    # For SRT Transcription
-    with open("temp_audio.wav", "rb") as audio_file:
-        response_srt = client.audio.transcriptions.create(
-            file=audio_file,
-            model="whisper-1",
-            response_format="srt",
-        )
 
-    # Directly use response_srt if it's already a string, which is expected for SRT content
-    if isinstance(response_srt, str):
-        srt_content = response_srt
-    else:
-        print("Unexpected SRT response format.")
-        srt_content = ""
+            self.progress.emit(int((i + 1) / n_chunks * 100))
 
-    # Parse and adjust the SRT content as needed
-    segments_list = list(srt.parse(srt_content))
-    for segment in segments_list:
-        segment.index += last_index
-        segment.start += datetime.timedelta(seconds=time_offset)
-        segment.end += datetime.timedelta(seconds=time_offset)
-        segment.start = segment.start - datetime.timedelta(microseconds=segment.start.microseconds)
-        segment.end = segment.end - datetime.timedelta(microseconds=segment.end.microseconds)
+        # After transcription is done or stopped, clean up temporary files
+        for temp_file in temp_files:
+            if os.path.exists(temp_file):
+                os.remove(temp_file)
 
-    modified_srt = srt.compose(segments_list)
-    transcription_srt += modified_srt
-    last_index = segments_list[-1].index + 1
-    time_offset += 30
+        if not self.stopped:
+            if self.transcribe_to_text:
+                txt_file_path = os.path.join(output_folder, os.path.splitext(os.path.basename(self.file_path))[0] + "_transcription.txt")
+                with open(txt_file_path, 'w', encoding='utf-8') as f:
+                    f.write(transcription_txt)
 
-    # Save the transcription as an .srt file
-    srt_file_path = os.path.join(transcription_data_folder, f"{output_name}.srt")
-    with open(srt_file_path, "w", encoding='utf-8') as f:
-        f.write(transcription_srt)
+            if self.transcribe_to_srt:
+                srt_file_path = os.path.join(output_folder, os.path.splitext(os.path.basename(self.file_path))[0] + "_transcription.srt")
+                with open(srt_file_path, 'w', encoding='utf-8') as f:
+                    f.write(transcription_srt)
 
-# Cleanup
-os.remove("temp_audio.wav")
 
-print("**** All Done! ****")
+            self.finished.emit("Transcription Complete. Saved to:", output_folder)
+
+
+    def stop(self):
+        self.stopped = True
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('WhisperCap')
+        self.setGeometry(100, 100, 600, 250)
+
+        self.layout = QVBoxLayout()
+        self.label = QLabel('No file selected', self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.txtCheckbox = QCheckBox('Transcribe to Text File (.txt)', self)
+        self.txtCheckbox.setChecked(True)
+        self.srtCheckbox = QCheckBox('Transcribe to SRT File (.srt)', self)
+        self.srtCheckbox.setChecked(True)
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.progressBar.setVisible(False)
+        self.loadButton = QPushButton('Load File', self)
+        self.loadButton.clicked.connect(self.loadFile)
+        self.transcribeButton = QPushButton('Start Transcription', self)
+        self.transcribeButton.clicked.connect(self.startTranscription)
+        self.transcribeButton.setEnabled(False)
+        self.stopButton = QPushButton('Stop', self)
+        self.stopButton.clicked.connect(self.stopTranscription)
+        self.stopButton.setEnabled(False)
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.txtCheckbox)
+        self.layout.addWidget(self.srtCheckbox)
+        self.layout.addWidget(self.progressBar)
+        self.layout.addWidget(self.loadButton)
+        self.layout.addWidget(self.transcribeButton)
+        self.layout.addWidget(self.stopButton)
+
+        self.setLayout(self.layout)
+
+        self.file_path = ''
+        self.transcriptionThread = None
+        self.txtCheckbox.stateChanged.connect(self.enableTranscriptionButton)
+        self.srtCheckbox.stateChanged.connect(self.enableTranscriptionButton)
+
+    def loadFile(self):
+        filePath, _ = QFileDialog.getOpenFileName(self, "Select Media File", "", "Media Files (*.mp4 *.wav *.mp3);;All Files (*)")
+        if filePath:
+            self.file_path = filePath
+            fileType = 'video' if os.path.splitext(filePath)[1].lower() == '.mp4' else 'audio'
+            self.label.setText(f"Loaded: {filePath}\nType: {fileType}")
+            self.transcribeButton.setEnabled(True)
+            self.progressBar.setValue(0)
+            self.progressBar.setVisible(False)
+            self.stopButton.setEnabled(False)
+
+    def enableTranscriptionButton(self):
+        self.transcribeButton.setEnabled(self.txtCheckbox.isChecked() or self.srtCheckbox.isChecked())
+
+    def startTranscription(self):
+        if self.file_path:
+            self.progressBar.setVisible(True)
+            self.transcribeButton.setEnabled(False)
+            self.stopButton.setEnabled(True)
+            self.transcriptionThread = TranscriptionThread(self.file_path, self.txtCheckbox.isChecked(), self.srtCheckbox.isChecked())
+            self.transcriptionThread.progress.connect(self.updateProgress)
+            self.transcriptionThread.finished.connect(self.transcriptionFinished)
+            self.transcriptionThread.errorOccurred.connect(self.showError)
+            self.transcriptionThread.start()
+
+    def stopTranscription(self):
+        if self.transcriptionThread and self.transcriptionThread.isRunning():
+            self.transcriptionThread.stop()
+            self.stopButton.setEnabled(False)
+            self.progressBar.setVisible(False)
+            self.progressBar.setValue(0)
+            self.label.setText("Transcription stopped by user.")
+
+    def enableTranscriptionButton(self):
+        self.transcribeButton.setEnabled(self.txtCheckbox.isChecked() or self.srtCheckbox.isChecked())
+
+    def updateProgress(self, value):
+        self.progressBar.setValue(value)
+
+    def transcriptionFinished(self, message, folder_path):
+        self.label.setText(f"{message}\n{folder_path}")
+        self.progressBar.setValue(100)
+        self.loadButton.setEnabled(True)
+        self.transcribeButton.setEnabled(False)
+        self.stopButton.setEnabled(False)  # Disable stop button after finishing
+
+    def showError(self, message):
+        self.label.setText(f"Error: {message}")
+        self.progressBar.setValue(0)
+        self.loadButton.setEnabled(True)
+        self.transcribeButton.setEnabled(True)
+        self.stopButton.setEnabled(False)  # Disable stop button on error
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec())
